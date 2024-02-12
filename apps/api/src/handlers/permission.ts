765023ae62482b9728337ed128c82a28a2e5ff2c
liuyu <russell44@126.com>
2024-02-12 14:38:58 +0800

    add reference

--- apps/api/src/handlers/permission.ts
+++ apps/api/src/handlers/permission.ts
@@ -1,3 +1,4 @@
+// <reference path="../../types/express/index.d.ts" />
 import { RequestHandler } from "express"
 import prisma from "../libs/prisma"
 import { PickOptional } from "shared-types"
--- apps/api/src/types/express/index.ts
+++ apps/api/src/types/express/index.ts
@@ -3,7 +3,7 @@ declare global {
   namespace Express {
     export interface User extends UserForAuthorization {}
     export interface Request {
-      user?: User
+      user?: UserForAuthorization
     }
   }
 }
