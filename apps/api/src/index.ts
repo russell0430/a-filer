0be18ea9a86e09fdfb1efc734ae4ef4debf2c130
liuyu <russell44@126.com>
2024-02-12 18:26:42 +0800

    debug

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -13,35 +13,32 @@ const resolvedPath = path.resolve(
 )
 
 const PUB_KEY = fs.readFileSync(resolvedPath, "utf-8")
-// const app = express()
-// app.use(cors({ cors: "*" }))
-// app.use(express.json())
-// //   passport.use()
-// app.use(passport.initialize())
-// passport.use(
-//   constructStrategy({
-//     strategy: "JWT",
-//     strategyOptions: {
-//       secretOrKey: PUB_KEY,
-//       algorithms: ["RS256"],
-//       jwtFromRequest: ExtractJwt.fromHeader("authorization"),
-//     },
-//   })
-// )
-// passport.use(
-//   constructStrategy({
-//     strategy: "LOCAL",
-//   })
-// )
-// app.use(router)
-// app.get("/", (req, res) => {
-//   res.send("hello world")
-// })
+const app = express()
+app.use(cors({ cors: "*" }))
+app.use(express.json())
+//   passport.use()
+app.use(passport.initialize())
+passport.use(
+  constructStrategy({
+    strategy: "JWT",
+    strategyOptions: {
+      secretOrKey: PUB_KEY,
+      algorithms: ["RS256"],
+      jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+    },
+  })
+)
+passport.use(
+  constructStrategy({
+    strategy: "LOCAL",
+  })
+)
+app.use(router)
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
 
 // static files
 // app.use(express.static(path.resolve(__dirname, "../../resources")))
-const app: RequestHandler = (req, res) => {
-  const { name = "World" } = req.query
-  res.send(`Hello ${name}!`)
-}
+
 export default app
