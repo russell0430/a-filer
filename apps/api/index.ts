9152cafe037ecffeee9e8ce83bec77bc5b37fdec
liuyu <russell44@126.com>
2024-02-12 18:02:48 +0800

    debug app

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -7,41 +7,38 @@ import router from "./src/router"
 import { constructStrategy } from "./src/libs/auth"
 import { ExtractJwt } from "passport-jwt"
 
-// const resolvedPath = path.resolve(
-//   __dirname,
-//   "./libs/cryptography/id_rsa_pub.pem"
-// )
+const resolvedPath = path.resolve(
+  __dirname,
+  "./src/libs/cryptography/id_rsa_pub.pem"
+)
 
-// const PUB_KEY = fs.readFileSync(resolvedPath, "utf-8")
-// const app = express()
-// app.use(cors({ cors: "*" }))
-// app.use(express.json())
-// //   passport.use()
-// app.use(passport.initialize())
-// passport.use(
-//   constructStrategy({
-//     strategy: "JWT",
-//     strategyOptions: {
-//       secretOrKey: PUB_KEY,
-//       algorithms: ["RS256"],
-//       jwtFromRequest: ExtractJwt.fromHeader("authorization"),
-//     },
-//   })
-// )
-// passport.use(
-//   constructStrategy({
-//     strategy: "LOCAL",
-//   })
-// )
-// app.use(router)
-// app.get("/", (req, res) => {
-//   res.send("hello world")
-// })
+const PUB_KEY = fs.readFileSync(resolvedPath, "utf-8")
+const app = express()
+app.use(cors({ cors: "*" }))
+app.use(express.json())
+//   passport.use()
+app.use(passport.initialize())
+passport.use(
+  constructStrategy({
+    strategy: "JWT",
+    strategyOptions: {
+      secretOrKey: PUB_KEY,
+      algorithms: ["RS256"],
+      jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+    },
+  })
+)
+passport.use(
+  constructStrategy({
+    strategy: "LOCAL",
+  })
+)
+app.use(router)
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
 
 // static files
 // app.use(express.static(path.resolve(__dirname, "../../resources")))
-const app: RequestHandler = (req, res) => {
-  const { name = "World" } = req.query
-  res.send(`Hello ${name}!`)
-}
+
 export default app
