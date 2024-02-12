29a0256370acf0eb1abce7d95ea40cd60d8b6f6f
liuyu <russell44@126.com>
2024-02-12 17:07:22 +0800

    modify apps/main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,37 +13,35 @@ const resolvedPath = path.resolve(
 )
 
 const PUB_KEY = fs.readFileSync(resolvedPath, "utf-8")
-async function main() {
-  const app = express()
-  app.use(cors({ cors: "*" }))
-  app.use(express.json())
-  //   passport.use()
-  app.use(passport.initialize())
-  passport.use(
-    constructStrategy({
-      strategy: "JWT",
-      strategyOptions: {
-        secretOrKey: PUB_KEY,
-        algorithms: ["RS256"],
-        jwtFromRequest: ExtractJwt.fromHeader("authorization"),
-      },
-    })
-  )
-  passport.use(
-    constructStrategy({
-      strategy: "LOCAL",
-    })
-  )
-  app.use(router)
-  app.use("/index", (req, res) => {
-    res.send("hello world")
+const app = express()
+app.use(cors({ cors: "*" }))
+app.use(express.json())
+//   passport.use()
+app.use(passport.initialize())
+passport.use(
+  constructStrategy({
+    strategy: "JWT",
+    strategyOptions: {
+      secretOrKey: PUB_KEY,
+      algorithms: ["RS256"],
+      jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+    },
   })
+)
+passport.use(
+  constructStrategy({
+    strategy: "LOCAL",
+  })
+)
+app.use(router)
+app.get("/index", (req, res) => {
+  res.send("hello world")
+})
 
-  // static files
-  // app.use(express.static(path.resolve(__dirname, "../../resources")))
-  app.listen(3000, () => {
-    console.log("listening on port 3000")
-  })
-  return app
-}
-export default main()
+// static files
+// app.use(express.static(path.resolve(__dirname, "../../resources")))
+app.listen(8080, () => {
+  console.log("listening on port 8080")
+})
+
+export default app
