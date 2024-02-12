1f2a03b30e66bbd3c9b29e39aaad830b318d26fe
liuyu <russell44@126.com>
2024-02-12 17:41:44 +0800

    debug

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -7,12 +7,12 @@ import router from "./src/router"
 import { constructStrategy } from "./src/libs/auth"
 import { ExtractJwt } from "passport-jwt"
 
-const resolvedPath = path.resolve(
-  __dirname,
-  "./libs/cryptography/id_rsa_pub.pem"
-)
+// const resolvedPath = path.resolve(
+//   __dirname,
+//   "./libs/cryptography/id_rsa_pub.pem"
+// )
 
-const PUB_KEY = fs.readFileSync(resolvedPath, "utf-8")
+// const PUB_KEY = fs.readFileSync(resolvedPath, "utf-8")
 // const app = express()
 // app.use(cors({ cors: "*" }))
 // app.use(express.json())
